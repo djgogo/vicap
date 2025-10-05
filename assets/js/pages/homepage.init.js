9b16b3a1f70a12e26f9b6b358d4bb92514abfb14
Peter Sacco <petersacco@websitemaster.ch>
2025-10-05 16:23:05 +0200

    landingpage completed

--- assets/js/pages/homepage.init.js
+++ assets/js/pages/homepage.init.js
@@ -1,9 +1,9 @@
-if (document.querySelectorAll('.wow').length > 0) {
-    const wow = new WOW({
-        boxClass: 'wow', // animated element css class (default is 'wow')
-        animateClass: 'animated', // animation css class (default is 'animated')
-        mobile: true, // trigger animations on mobile devices (default is true)
-        live: true // act on asynchronously loaded content (default is true)
-    });
-    wow.init();
-}
+// if (document.querySelectorAll('.wow').length > 0) {
+//     const wow = new WOW({
+//         boxClass: 'wow', // animated element css class (default is 'wow')
+//         animateClass: 'animated', // animation css class (default is 'animated')
+//         mobile: true, // trigger animations on mobile devices (default is true)
+//         live: true // act on asynchronously loaded content (default is true)
+//     });
+//     wow.init();
+// }
